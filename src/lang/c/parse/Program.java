4f2b7fc4749129f77fd7dd71f4f6b437ab40922d
Okupedia <okupedia15@gmail.com>
2021-01-29 13:04:42 +0900

    H7:チェック済み

--- src/lang/c/parse/Program.java
+++ src/lang/c/parse/Program.java
@@ -52,7 +52,7 @@ public class Program extends CParseRule {
 			for(var statement:statementList){
 				statement.codeGen(pcx);
 			}
-			o.println("\tMOV\t-(R6), R0\t; ProgramNode: 計算結果確認用");
+			//o.println("\tMOV\t-(R6), R0\t; ProgramNode: 計算結果確認用");
 		}
 		o.println("\tHLT\t\t\t; ProgramNode:");
 		o.println("\t.END\t\t\t; ProgramNode:");
